f31012b49991090462b9de03a70195561adf2e6e
n464tongue <sps32qc9@gmail.com>
2025-07-07 00:49:39 +0900

    backgrand color changed

--- src/home/PackWrapper.js
+++ src/home/PackWrapper.js
@@ -1,6 +1,21 @@
 const PackWrapper = ({ children }) => {
   return (
-    <div class="flex space-x-4 items-center justify-center rounded-xl min-h-screen w-screen shadow-lg text-white bg-[radial-gradient(ellipse_at_bottom_center,_var(--tw-gradient-stops))] from-white via-gray-300 to-gray-500 bg-[length:120%_100%]">
+    <div class="
+      flex
+      space-x-4
+      items-center
+      justify-center
+      rounded-xl
+      min-h-screen
+      w-screen
+      shadow-lg
+      text-white
+      bg-[radial-gradient(ellipse_at_bottom_center,_var(--tw-gradient-stops))]
+      from-white
+      via-gray-200
+      to-gray-500
+      bg-[length:120%_100%]"
+    >
       {children}
     </div>
   );
